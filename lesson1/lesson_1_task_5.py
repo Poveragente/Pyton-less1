9bd73833c43326e83949efc4374da84717d74bee
Poveragente <154063416+Poveragente@users.noreply.github.com>
2026-07-18 14:35:39 +0300

    task_5 исправление

--- lesson1/lesson_1_task_5.py
+++ lesson1/lesson_1_task_5.py
@@ -1,10 +1,11 @@
 num = input("Введите номер: ")
 num = "88005553535" 
-#Делаем из строчки массив 
-def get_part(index: int) -> str:
-    if 0 <= index < len(num):
-        return num[index]
-    return ""
-# Вызовы: каждый выводит свою часть (один символ)
-for i in range(11):
-    print(get_part(i))
+
+def get_part(num):
+    print(num)
+
+for _ in range(11):
+    get_part(num)
+
+
+
